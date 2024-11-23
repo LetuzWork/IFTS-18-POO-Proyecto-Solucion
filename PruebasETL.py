8001a538f310dc4f9837a3126106fe64fc99729c
Mercedes Marighetti <anamercedes.marighetti@bbva.com>
2024-11-23 15:04:18 -0300

    Aca mando los avances en Limpieza.py y algo de lo mio en Pruebas ETL. Elimino el archivo empleados.py que no tenia que ver con el trabajo.

--- PruebasETL.py
+++ PruebasETL.py
@@ -1,15 +1,30 @@
 import pandas as pd
+from peewee import *
 
-# Supongamos que tienes un DataFrame llamado df
-df = pd.DataFrame({
-    'columna1': ['valor1', 'valor2', 'valor3'],
-    'columna2': ['valor4', 'valor5', 'valor6']
-})
+'''df = pd.read_csv('https://cdn.buenosaires.gob.ar/datosabiertos/datasets/secretaria-general-y-relaciones-internacionales/ba-obras/observatorio-de-obras-urbanas.csv', sep=";", index_col=0, encoding='latin-1')
 
-# Reemplazar 'valor1' por 'nuevo_valor1' en todo el DataFrame
-df = df.replace('valor1', 'nuevo_valor1')
+camposdeInteres = ['nombre', 'etapa', 'tipo', 'area_responsable', 'descripcion',
+       'monto_contrato', 'comuna', 'barrio', 'direccion', 'fecha_inicio', 'fecha_fin_inicial', 'plazo_meses', 'porcentaje_avance',
+       'licitacion_oferta_empresa', 'licitacion_anio', 'contratacion_tipo',
+       'nro_contratacion', 'cuit_contratista', 
+      'expediente-numero', 'financiamiento']  # Replace with your actual column names
+df = df[camposdeInteres]
 
-# Reemplazar múltiples valores en una columna específica
-df['columna1'] = df['columna1'].replace({'valor2': 'nuevo_valor2', 'valor3': 'nuevo_valor3'})
+print(df.head())
+df.to_csv('camposdeinteres.csv', sep=';', encoding='latin-1')'''
 
-print(df)
+dfOK = pd.read_csv('camposdeinteres.csv', sep=';', index_col=0, encoding='latin-1')
+mycols = ['nro_contratacion', 'expediente-numero', 'financiamiento','mano_obra']
+
+dfmycols = dfOK[mycols]
+dfOK[mycols].dropna(how='all', inplace=True)
+
+for column in dfmycols.columns:
+    unique_values = dfOK[column].unique()
+    print(f"Los valores unicos de la columna : '{column}': {unique_values}")
+
+dfmycols = dfmycols.fillna({'financiamiento': 'No especificado', 'mano_obra': '0'})
+
+
+print(dfmycols.head(), dfmycols.shape)
+
