7d0a37a5c41672d70b75e0d8e14166e4f1135ec1
Mercedes Marighetti <anamercedes.marighetti@bbva.com>
2024-11-23 15:43:38 -0300

    Agrego lo de Diego a Limpieza.py

--- Limpieza.py
+++ Limpieza.py
@@ -159,5 +159,30 @@ df["contratacion_tipo"] = df["contratacion_tipo"].str.replace('-', '') # Removie
 # Es un numero unico por obra pero con formato que varia entre obras, inicialmente sin formatear
 
 
+#AGREGO LIMPIEZA DIEGO:
+# 'descripcion' 'monto_contrato' 'comuna' 'barrio' 'direccion'
+
+### 'descripcion', 'barrio', 'direccion' ###  Eliminados espacios inncesarios y convertidos todos a minusculas
+
+df['descripcion'] = df['descripcion'].fillna('Indeterminado').str.strip().str.lower()
+
+df['barrio'] = df['barrio'].fillna('Indeterminado').str.strip().str.lower()
+
+df['direccion'] = df['direccion'].fillna('Indeterminado').str.strip().str.lower()
+
+### monto_contrato ### 
+df['monto_contrato'] = df['monto_contrato'].str.replace(r'[$.]', '', regex=True) #Reemplaza las signos '$' y '.' por vacios
+
+df['monto_contrato'] = df['monto_contrato'].str.replace(r'(?<=\d),(?=\d{3})', '', regex=True, n=2) #Elimina las comas que son separadores de miles y elimina solo las 2 primeras coincidencias
+
+df['monto_contrato'] = df['monto_contrato'].str.replace(',', '.') # Reemplaza las comas por puntos
+
+df['monto_contrato'] = df['monto_contrato'].str.replace(r'\s*\(.*?\)', '', regex=True) # Borra los datos que estan entre parentesis
+
+df['monto_contrato'] = df['monto_contrato'].fillna('0.00').str.strip().str.extract(r'[-+]?\$?([\d.,]+)').replace({',': ''}, regex=True).astype(float) # Extrae los primeros datos numericos de las oraciones y los convierte en datos validos
+
+
+### comuna ### Convertido a numeros 
+df['comuna'] = pd.to_numeric(df['comuna'], errors= 'coerce').fillna(0).astype(int)
 
 
