bed23bb1a8e7e34fbd1947402eecac93aa7e9c76
Mercedes Marighetti <anamercedes.marighetti@bbva.com>
2024-11-23 12:11:21 -0300

    Agrego lo de Marto a Limpieza.py

--- Limpieza.py
+++ Limpieza.py
@@ -122,3 +122,42 @@ areas.to_csv("areas_responsables.csv", index=False) # tabla con el area
 obras.to_csv("obras.csv", index=False) # tabla principal que une nombres de las obras y id del tipo, etapa y área
 
 print("Tablas normalizadas.")
+
+import pandas as pd
+
+df = pd.read_csv('https://cdn.buenosaires.gob.ar/datosabiertos/datasets/secretaria-general-y-relaciones-internacionales/ba-obras/observatorio-de-obras-urbanas.csv', sep=";", index_col=0, encoding='latin-1')
+
+# Este Archivo es provisional y será mergeado con los demas grupos
+# El Grupo 4 serian "licitacion_oferta_empresa", "cuit_contratista", "licitacion_anio", "contratacion_tipo","nro_contratacion"
+##LIMPIEZA MARTO
+##### licitacion_oferta_empresa #####
+df["licitacion_oferta_empresa"] = df["licitacion_oferta_empresa"].str.strip() # Removiendo espacios en blanco al principio y al final
+
+# data para poblar tablas
+empresas = df["licitacion_oferta_empresa"].dropna().unique() # Empresas unicas
+
+##### cuit_contratista #####
+
+df["cuit_contratista"] = df["cuit_contratista"].astype(str) # Convirtiendo a string
+# Estandarizando el separador
+df["cuit_contratista"] = df["cuit_contratista"].str.replace(r'[\n;]', ',', regex=True) 
+# Removiendo cualquier no numero excepto comas
+df["cuit_contratista"] = df["cuit_contratista"].str.replace(r'[^0-9,]', '', regex=True)
+
+##### licitacion_anio #####
+df["licitacion_anio"] = df["licitacion_anio"].astype(str)
+# Removiendo cualquier no numero
+df["licitacion_anio"] = df["licitacion_anio"].str.replace(r'[^0-9]', '', regex=True)
+
+##### contratacion_tipo #####
+df["contratacion_tipo"] = df["contratacion_tipo"].astype(str)
+
+#df["contratacion_tipo"] = df["contratacion_tipo"].str.replace(r'[-]', '', regex=True)
+df["contratacion_tipo"] = df["contratacion_tipo"].str.replace('-', '') # Removiendo guiones sin Regex
+
+##### nro_contratacion #####
+# Es un numero unico por obra pero con formato que varia entre obras, inicialmente sin formatear
+
+
+
+
