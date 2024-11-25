e5ab11266171e255183a26731e057743c5dbf242
Ludmila Brizuela <brizuelal127@gmail.com>
2024-11-25 13:07:58 -0300

    Corrijo error de indentación en obtener_indicadores

--- gestionar_obras.py
+++ gestionar_obras.py
@@ -193,77 +193,77 @@ class GestionarObra(metaclass=ABCMeta):
         def obtener_indicadores(cls):
             db = cls.conectar_db()
 
-        try:
-            # a. Listado de todas las áreas responsables
-            print('\nListado de áreas responsables:')
-            for area in orm.AreaResponsable.select():
-                print(area)
+            try:
+                # a. Listado de todas las áreas responsables
+                print('\nListado de áreas responsables:')
+                for area in orm.AreaResponsable.select():
+                    print(area)
 
-            # b. Listado de todos los tipos de obra
-            print('\nListado de tipos de obra:')
-            for tipo_obra in orm.TipoObra.select():
-                print(tipo_obra)
+                # b. Listado de todos los tipos de obra
+                print('\nListado de tipos de obra:')
+                for tipo_obra in orm.TipoObra.select():
+                    print(tipo_obra)
 
-            # c. Cantidad de obras que se encuentran en cada etapa
-            print('\nCantidad de obras por etapa:')
-            for etapa in orm.Etapa.select():
-                cantidad_obras = orm.Obra.select().where(orm.Obra.etapa == etapa).count()
-                print(f'{etapa.estado}: {cantidad_obras} obras')
+                # c. Cantidad de obras que se encuentran en cada etapa
+                print('\nCantidad de obras por etapa:')
+                for etapa in orm.Etapa.select():
+                    cantidad_obras = orm.Obra.select().where(orm.Obra.etapa == etapa).count()
+                    print(f'{etapa.estado}: {cantidad_obras} obras')
 
-            # d. Cantidad de obras y monto total de inversión por tipo de obra
-            print('\nCantidad de obras y monto total de inversión por tipo de obra:')
-            for tipo_obra in orm.TipoObra.select():
-                obras = orm.Obra.select().where(orm.Obra.tipo_obra == tipo_obra)
-                cantidad_obras = obras.count()
-                monto_total = obras.select(fn.SUM(orm.Obra.monto_contrato)).scalar() or 0
+                # d. Cantidad de obras y monto total de inversión por tipo de obra
+                print('\nCantidad de obras y monto total de inversión por tipo de obra:')
+                for tipo_obra in orm.TipoObra.select():
+                    obras = orm.Obra.select().where(orm.Obra.tipo_obra == tipo_obra)
+                    cantidad_obras = obras.count()
+                    monto_total = obras.select(fn.SUM(orm.Obra.monto_contrato)).scalar() or 0
+                    
+                    print(f'-{tipo_obra.tipo}: {cantidad_obras} obras, Monto total de inversión: ${monto_total:,.2f}')
                 
-                print(f'-{tipo_obra.tipo}: {cantidad_obras} obras, Monto total de inversión: ${monto_total:,.2f}')
-            
-            # e. Listado de todos los barrios pertenecientes a las comunas 1, 2 y 3
-            print('\nListado de todos los barrios pertenecientes a las comunas 1, 2 y 3:')
-            barrios = orm.Barrio.select().where(orm.Barrio.comuna <= 3)
-            
-            print('\n'.join(barrio.nombre for barrio in barrios))
-            
-            # f. Cantidad de obras finalizadas y su monto total de inversión en la comuna 1
-            print('\nObras finalizadas y su monto total de inversión en la comuna 1:')
-            obras_finalizadas_c1 = orm.Obra.select().join(orm.Barrio).join(orm.Etapa).where((orm.Etapa.nombre == 'Finalizada') & (orm.Barrio.comuna == 1))
-            cantidad_obras_c1 = obras_finalizadas_c1.count()
-            monto_total_c1 = obras_finalizadas_c1.select(fn.SUM(orm.Obra.monto_contrato)).scalar() or 0
-            
-            print(f'Cantidad de obras: {cantidad_obras_c1}')
-            print(f'Monto total de inversión: ${monto_total_c1:,.2f}')
+                # e. Listado de todos los barrios pertenecientes a las comunas 1, 2 y 3
+                print('\nListado de todos los barrios pertenecientes a las comunas 1, 2 y 3:')
+                barrios = orm.Barrio.select().where(orm.Barrio.comuna <= 3)
+                
+                print('\n'.join(barrio.nombre for barrio in barrios))
+                
+                # f. Cantidad de obras finalizadas y su monto total de inversión en la comuna 1
+                print('\nObras finalizadas y su monto total de inversión en la comuna 1:')
+                obras_finalizadas_c1 = orm.Obra.select().join(orm.Barrio).join(orm.Etapa).where((orm.Etapa.nombre == 'Finalizada') & (orm.Barrio.comuna == 1))
+                cantidad_obras_c1 = obras_finalizadas_c1.count()
+                monto_total_c1 = obras_finalizadas_c1.select(fn.SUM(orm.Obra.monto_contrato)).scalar() or 0
+                
+                print(f'Cantidad de obras: {cantidad_obras_c1}')
+                print(f'Monto total de inversión: ${monto_total_c1:,.2f}')
 
-            # g. Cantidad de obras finalizadas en un plazo menor o igual a 24 meses
-            print('\nCantidad de obras finalizadas en un plazo menor o igual a 24 meses:')
-            obras_finalizadas_plazo = orm.Obra.select().join(orm.Etapa).where((orm.Etapa.nombre == 'Finalizada') & (orm.Obra.plazo_meses <= 24)).count()
+                # g. Cantidad de obras finalizadas en un plazo menor o igual a 24 meses
+                print('\nCantidad de obras finalizadas en un plazo menor o igual a 24 meses:')
+                obras_finalizadas_plazo = orm.Obra.select().join(orm.Etapa).where((orm.Etapa.nombre == 'Finalizada') & (orm.Obra.plazo_meses <= 24)).count()
+                
+                print(f'\nObras finalizadas en un plazo menor o igual a 24 meses: {obras_finalizadas_plazo}')
             
-            print(f'\nObras finalizadas en un plazo menor o igual a 24 meses: {obras_finalizadas_plazo}')
-        
-            # h. Porcentaje total de obras finalizadas
-            print('\nPorcentaje total de obras finalizadas:')
-            total_obras = orm.Obra.select().count()
-            obras_finalizadas = orm.Obra.select().join(orm.Etapa).where(orm.Etapa.nombre == 'Finalizada').count()
-            porcentaje_obras_finalizadas = (obras_finalizadas / total_obras) * 100 if total_obras > 0 else 0
-            
-            print(f'\nPorcentaje de obras finalizadas: {porcentaje_obras_finalizadas:.2f}%')
+                # h. Porcentaje total de obras finalizadas
+                print('\nPorcentaje total de obras finalizadas:')
+                total_obras = orm.Obra.select().count()
+                obras_finalizadas = orm.Obra.select().join(orm.Etapa).where(orm.Etapa.nombre == 'Finalizada').count()
+                porcentaje_obras_finalizadas = (obras_finalizadas / total_obras) * 100 if total_obras > 0 else 0
+                
+                print(f'\nPorcentaje de obras finalizadas: {porcentaje_obras_finalizadas:.2f}%')
 
-            # i. Cantidad total de mano de obra empleada
-            print('\nCantidad total de mano de obra empleada:')
-            cantidad_total = orm.Obra.select(fn.SUM(orm.Obra.mano_obra)).scalar() or 0
-            
-            print(f'\nCantidad total de mano de obra empleada: {cantidad_total:,}')
+                # i. Cantidad total de mano de obra empleada
+                print('\nCantidad total de mano de obra empleada:')
+                cantidad_total = orm.Obra.select(fn.SUM(orm.Obra.mano_obra)).scalar() or 0
+                
+                print(f'\nCantidad total de mano de obra empleada: {cantidad_total:,}')
 
-            # j. Monto total de inversión
-            print('\nMonto total de inversión:')
-            monto_total_inversion = orm.Obra.select(fn.SUM(orm.Obra.monto_contrato)).scalar() or 0
-            
-            print(f"Monto total de inversion: ${monto_total_inversion:,.2f}")
+                # j. Monto total de inversión
+                print('\nMonto total de inversión:')
+                monto_total_inversion = orm.Obra.select(fn.SUM(orm.Obra.monto_contrato)).scalar() or 0
+                
+                print(f"Monto total de inversion: ${monto_total_inversion:,.2f}")
 
-        except Exception as e:
-            print(f"Error al obtener indicadores: {e}")
-        finally:
-            db.close()
+            except Exception as e:
+                print(f"Error al obtener indicadores: {e}")
+            finally:
+                db.close()
 
 if __name__ == '__main__':
     #GestionarObra().mapear_orm()
